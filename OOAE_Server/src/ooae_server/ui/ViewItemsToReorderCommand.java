66ace1d7a005376878b286f14ca43e53d039cd8c
efarioli <efarioli@gmail.com>
2020-03-23 18:02:13 +0000

    Implementing Command Pattern 12 - ViewItemsToReorderCommand
    ViewItemsToReorderCommand

---
+++ OOAE_Server/src/ooae_server/ui/ViewItemsToReorderCommand.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ooae_server.ui;
+
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import ooae_server.DTO_Factory;
+import ooae_server.entity.Item;
+
+/**
+ *
+ * @author f023507i
+ */
+public class ViewItemsToReorderCommand implements Command
+{
+
+    private String outStr;
+
+    public ViewItemsToReorderCommand(String outStr)
+    {
+        this.outStr = outStr;
+    }
+
+    
+    @Override
+    public String execute()
+    {
+        try
+        {
+            ArrayList<Item> itemsToReorder = Item.findItemsToReorder();
+
+            outStr = new Gson().toJson(DTO_Factory.create(itemsToReorder));
+        } catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+            outStr = "ERROR: Unrecognised command";
+        }
+        return outStr;
+    }
+
+}
