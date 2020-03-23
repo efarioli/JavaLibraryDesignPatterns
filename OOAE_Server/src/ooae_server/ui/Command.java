883a8d2e0df338cc6acedf00fa5e8d5ba6c1b81c
efarioli <efarioli@gmail.com>
2020-03-23 17:17:06 +0000

    Implementing Command Pattern - creating Command inteface

---
+++ OOAE_Server/src/ooae_server/ui/Command.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ooae_server.ui;
+
+/**
+ *
+ * @author f023507i
+ */
+public interface Command
+{
+    String execute();
+}
