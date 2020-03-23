77e94ec6a19e0c72ff453e9653711db2c2a5a758
efarioli <efarioli@gmail.com>
2020-03-23 18:28:10 +0000

    Finishing Implementing Command Pattern using Factory Pattern as well
    Replacing long switch statement for  the Factory that uses the Command pattern

--- OOAE_Server/src/ooae_server/AdminClientResponder.java
+++ OOAE_Server/src/ooae_server/AdminClientResponder.java
@@ -1,16 +1,9 @@
 package ooae_server;
 
-import com.google.gson.Gson;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
-import ooae_library.data_transfer_object.ItemDTO;
 import ooae_server.database.AdminClientResponderGateway;
-import ooae_server.entity.*;
-import ooae_server.database.CustomerGateway;
-import ooae_server.database.ItemGateway;
-import ooae_server.database.OrderGateway;
-import ooae_server.database.SupplierGateway;
+import ooae_server.ui.CommandFactory;
 
 /**
  *
@@ -45,74 +38,7 @@ public class AdminClientResponder implements Runnable
         String outStr = "OK";
         try
         {
-            switch (command)
-            {
-                case "addItem":
-                    ItemDTO item1 = new Gson().fromJson(jsonInStr, ItemDTO.class);
-
-                    Item item = new Item(item1);
-
-                    Item insertedItem = item.insert();
-
-                    outStr = new Gson().toJson(DTO_Factory.create(insertedItem));
-                    break;
-
-                case "checkNeedForDatabaseInitialisation":
-                    checkNeedForDatabaseInitialisation();
-                    break;
-
-                case "initialiseDatabase":
-                    initialiseDatabase();
-                    break;
-
-                case "shipOrder":
-                    int orderId = new Gson().fromJson(jsonInStr, Integer.class);
-
-                    Order order = Order.findOrder(orderId);
-                    Order shippedOrder = null;
-
-                    if (order != null)
-                    {
-                        shippedOrder = order.ship();
-                    }
-
-                    outStr = new Gson().toJson(DTO_Factory.create(shippedOrder, null));
-                    break;
-
-                case "updateItemQuantityInStock":
-                    ItemDTO item2 = new Gson().fromJson(jsonInStr, ItemDTO.class);
-
-                    Item itemToUpdate = new Item(item2);
-
-                    Item updatedItem = itemToUpdate.updateItemQuantityInStock();
-
-                    outStr = new Gson().toJson(DTO_Factory.create(updatedItem));
-                    break;
-
-                case "viewAllItems":
-                    ArrayList<Item> allItems = Item.findAllItems();
-
-                    outStr = new Gson().toJson(DTO_Factory.create(allItems));
-                    break;
-
-                case "viewItem":
-                    int itemId = new Gson().fromJson(jsonInStr, Integer.class);
-
-                    Item itemToView = Item.findItem(itemId);
-
-                    outStr = new Gson().toJson(DTO_Factory.create(itemToView));
-                    break;
-
-                case "viewItemsToReorder":
-                    ArrayList<Item> itemsToReorder = Item.findItemsToReorder();
-
-                    outStr = new Gson().toJson(DTO_Factory.create(itemsToReorder));
-                    break;
-
-                default:
-                    outStr = "ERROR: Unrecognised command";
-                    break;
-            }
+            outStr = CommandFactory.createCommand(command, jsonInStr, outStr).execute();
             clientOut.println(outStr);
             clientOut.flush();
             if (socket != null)
--- OOAE_Server/src/ooae_server/CustomerClientResponder.java
+++ OOAE_Server/src/ooae_server/CustomerClientResponder.java
@@ -1,12 +1,8 @@
 package ooae_server;
 
-import com.google.gson.Gson;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.HashMap;
-import ooae_library.data_transfer_object.CustomerDTO;
-import ooae_server.entity.*;
+import ooae_server.ui.CommandFactory;
 
 /**
  *
@@ -40,72 +36,7 @@ public class CustomerClientResponder implements Runnable
         String outStr = "OK";
         try
         {
-            switch (command)
-            {
-                case "cancelOrder":
-                    int orderId = new Gson().fromJson(jsonInStr, Integer.class);
-
-                    Order order = Order.findOrder(orderId);
-                    Order cancelledOrder = null;
-
-                    if (order != null)
-                    {
-                        cancelledOrder = order.cancel();
-                    }
-
-                    outStr = new Gson().toJson(DTO_Factory.create(cancelledOrder, null));
-                    break;
-
-                case "login":
-                    CustomerDTO customerDTO = new Gson().fromJson(jsonInStr, CustomerDTO.class);
-
-                    Customer customer = new Customer(customerDTO);
-
-                    Customer user = customer.login();
-
-                    outStr = new Gson().toJson(DTO_Factory.create(user));
-                    break;
-
-                case "placeOrder":
-                    CustomerDTO customerDTO1 = new Gson().fromJson(jsonInStr, CustomerDTO.class);
-
-                    Customer customer1 = new Customer(customerDTO1);
-
-                    Order orderToInsert = customer1.getOrders().get(-1);
-
-                    Order insertedOrder = orderToInsert.insert();
-
-                    outStr = new Gson().toJson(DTO_Factory.create(insertedOrder, null));
-                    break;
-
-                case "viewAllItems":
-                    ArrayList<Item> allItems = Item.findAllItems();
-
-                    outStr = new Gson().toJson(DTO_Factory.create(allItems));
-                    break;
-
-                case "viewItem":
-                    int itemId = new Gson().fromJson(jsonInStr, Integer.class);
-
-                    Item itemToView = Item.findItem(itemId);
-
-                    outStr = new Gson().toJson(DTO_Factory.create(itemToView));
-                    break;
-
-                case "viewMyOrders":
-                    CustomerDTO customerDTO2 = new Gson().fromJson(jsonInStr, CustomerDTO.class);
-
-                    Customer customer2 = new Customer(customerDTO2);
-
-                    HashMap<Integer, Order> myOrders = Order.findOrdersForCustomer(customer2);
-
-                    outStr = new Gson().toJson(DTO_Factory.create(myOrders, null));
-                    break;
-
-                default:
-                    outStr = "ERROR: Unrecognised command";
-                    break;
-            }
+            outStr = CommandFactory.createCommand(command, jsonInStr, outStr).execute();
             clientOut.println(outStr);
             clientOut.flush();
             if (socket != null)
